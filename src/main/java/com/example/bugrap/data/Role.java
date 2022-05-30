9133135803a59f5b1792f2146a188ef99659a9c7
start.vaadin.com <no-reply@vaadin.com>
2022-05-30 10:15:11 +0000

    Generated project

---
+++ src/main/java/com/example/bugrap/data/Role.java
@@ -0,0 +1,5 @@
+package com.example.bugrap.data;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ src/main/java/com/example/bugrap/views/bugs/BugsView.java
@@ -0,0 +1,35 @@
+package com.example.bugrap.views.bugs;
+
+import com.example.bugrap.views.MainLayout;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import javax.annotation.security.PermitAll;
+
+@PageTitle("Bugs")
+@Route(value = "bugs", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+@PermitAll
+public class BugsView extends VerticalLayout {
+
+    public BugsView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
